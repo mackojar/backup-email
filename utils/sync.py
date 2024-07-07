f4d9e17509fbe716162f9053b893ff8251e3e77b
Maciej Jarkowski <mackoj@tlen.pl>
2024-07-08 00:07:44 +0200

    retrieve message ID using bulk fetch

--- utils/sync.py
+++ utils/sync.py
@@ -1,6 +1,7 @@
 import logging
 import mailbox
 import imaplib
+from typing import List, Tuple
 from .imap import checkResponse
 
 class MailSyncInfo:
@@ -10,13 +11,22 @@ class MailSyncInfo:
     self.existsInIMAP = existsInIMAP
 
 
-def _getMessageID(mail: imaplib.IMAP4_SSL, messageId) -> str:
-  status, message = mail.fetch(messageId, '(BODY[HEADER.FIELDS (MESSAGE-ID)])')
-  checkResponse(status, "retrieve mail Message-Id")
-  msgId = message[0][1]   # b'Message-ID: <xxxxx@mail.gmail.com>\r\n\r\n'
-  l = msgId.find(b'<')
-  r = msgId.find(b'>')
-  return msgId[l:r+1].decode()   # <xxxxx@mail.gmail.com>
+def _convertMsgId(imapMessageId: bytes): # b'Message-ID: <xxxxx@mail.gmail.com>\r\n\r\n'
+  l = imapMessageId.find(b'<')
+  r = imapMessageId.find(b'>')
+  return imapMessageId[l:r+1].decode()   # <xxxxx@mail.gmail.com>
+
+def _getMessageIDs(mail: imaplib.IMAP4_SSL, messageIds: list[bytes]) -> List[Tuple[bytes, str]]:
+  logging.info("Retrieve IMAP message IDs...")
+  criteria = messageIds[0] + b':' + messageIds[-1]
+  status, messages = mail.fetch(criteria, '(BODY[HEADER.FIELDS (MESSAGE-ID)])')
+  checkResponse(status, "retrieve mail Message-Ids")
+  messagesOnly = filter(lambda m: isinstance(m, tuple), messages)
+  result = []
+  for message in messagesOnly:
+    imapId = message[0].split(b' ')[0]
+    result.append( (imapId, _convertMsgId(message[1])))
+  return result
 
 def _createMsgsInfo(mbox: mailbox.mbox) -> dict[str, MailSyncInfo]:
   msgIdMap = {}
@@ -25,15 +35,19 @@ def _createMsgsInfo(mbox: mailbox.mbox) -> dict[str, MailSyncInfo]:
   return msgIdMap
 
 def _addMissingIMAPEmails(mail: imaplib.IMAP4_SSL, imapIds: list[bytes], mbox: mailbox.mbox, mboxMsgsInfo: dict[str, MailSyncInfo]):
+  if len(imapIds) == 0:
+    return
   logging.info("Retrieve missing IMAP emails...")
-  for imapId in imapIds:
-    logging.info(f"Check IMAP email {imapId}...")
-    messageId = _getMessageID(mail, imapId)
+  messageInfos = _getMessageIDs(mail, imapIds)
+  for messageInfo in messageInfos:
+    imapMsgId = messageInfo[0]
+    messageId = messageInfo[1]
+    logging.info(f"Check IMAP email {imapMsgId}...")
     if messageId in mboxMsgsInfo:
       mboxMsgsInfo[messageId].existsInIMAP = True
     else:
-      logging.info(f"Retrieving IMAP email {imapId}:{messageId}...")
-      status, msgData = mail.fetch(imapId, "(RFC822)")
+      logging.info(f"Retrieving IMAP email {imapMsgId}:{messageId}...")
+      status, msgData = mail.fetch(imapMsgId, "(RFC822)")
       checkResponse(status, "retrieve message")
       rawEmail = msgData[0][1]
       msg = mailbox.mboxMessage(rawEmail)
